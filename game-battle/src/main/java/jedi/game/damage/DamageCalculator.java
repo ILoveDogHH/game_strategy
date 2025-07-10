5cf3e678a02b55f6178ffe8b25d4c04e3a94a964
zhanglefeng <1157174605@qq.com>
2025-07-10 17:38:28 +0800

    cc

--- game-battle/src/main/java/jedi/game/damage/DamageCalculator.java
+++ game-battle/src/main/java/jedi/game/damage/DamageCalculator.java
@@ -23,10 +23,11 @@ public class DamageCalculator {
      * @return 实际造成的伤害
      */
     public static Action calculateDamage(BattleContext ctx, IEntity attacker, Player target, int baseDamage, DamageType damageType, ActionType actionType) {
-        double finalDamage = 0;
+
         Action action = new Action(ctx.getCurrentTime());
         List<IEntity> defenders = TargetSelector.selectTargets(attacker, target, attacker.getTargeType());
         for(IEntity defender : defenders){
+            double finalDamage = 0;
             // 计算出不同类型的伤害
             finalDamage = damageType.damageCalculator.calculate(ctx, attacker, defender, baseDamage);
             ActionDetail actionDetail = new ActionDetail( attacker, defender, actionType);
--- game-battle/src/main/java/jedi/game/enums/DamageType.java
+++ game-battle/src/main/java/jedi/game/enums/DamageType.java
@@ -5,42 +5,25 @@ import jedi.game.damage.*;
 // 枚举类，表示伤害类型及其特性
 public enum DamageType {
     // 物理伤害，可暴击，可闪避
-    PHYSICAL(true, true, new PhysicalDamageCalculator(), new PhysicalHitLogicHandler()),
+    PHYSICAL( new PhysicalDamageCalculator(), new PhysicalHitLogicHandler()),
     // 大招伤害，不可暴击，不可闪避
-    STRATEGY(false, false, new StrategyDamageCalculator(), new StrategyHitLogicHandler()),
+    STRATEGY( new StrategyDamageCalculator(), new StrategyHitLogicHandler()),
     // 燃烧伤害，不可暴击，不可闪避
-    BURN(false, false, new BurnDamageCalculator(), new BurnHitLogicHandler()),
+    BURN(new BurnDamageCalculator(), new BurnHitLogicHandler()),
     // 真实伤害，不可暴击，不可闪避
-    FATE(false, false, new FateDamageCalculator(), new FateHitLogicHandler()),
+    FATE(new FateDamageCalculator(), new FateHitLogicHandler()),
 
     ;
 
-    // 是否可以暴击
-    private final boolean canCrit;
-    // 是否可以闪避
-    private final boolean canDodge;
 
     public final IDamageCalculator damageCalculator;
 
     public final IHitLogicHandler iHitLogicHandler;
 
     // 构造方法，用于初始化伤害类型的特性
-    DamageType(boolean canCrit, boolean canDodge, IDamageCalculator damageCalculator, IHitLogicHandler iHitLogicHandler) {
-        this.canCrit = canCrit;
-        this.canDodge = canDodge;
+    DamageType( IDamageCalculator damageCalculator, IHitLogicHandler iHitLogicHandler) {
         this.damageCalculator = damageCalculator;
         this.iHitLogicHandler = iHitLogicHandler;
     }
 
-
-
-    // 获取是否可以暴击
-    public boolean canCrit() {
-        return canCrit;
-    }
-
-    // 获取是否可以闪避
-    public boolean canDodge() {
-        return canDodge;
-    }
 }
